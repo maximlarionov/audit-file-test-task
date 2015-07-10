259e28612df3d9aab119c2ca0d1037cb9319f958
maximlarionov <maxim.larionov@flatstack.com>
2015-07-10 19:00:06 +0300

    Added carrierwave docs

---
+++ app/controllers/documents_controller.rb
@@ -0,0 +1,27 @@
+class DocumentsController < ApplicationController
+  respond_to :html
+
+  expose(:document, attributes: :document_params)
+  expose(:documents) { Document.page(params[:page]) }
+
+  def create
+    flash[:notice] = 'Document was successfully created.' if document.save
+    respond_with(document)
+  end
+
+  def update
+    flash[:notice] = 'Document was successfully updated.' if document.save
+    respond_with(document)
+  end
+
+  def destroy
+    document.destroy
+    respond_with(document)
+  end
+
+  private
+
+  def document_params
+    params.require(:document).permit(:name, :attachment)
+  end
+end
