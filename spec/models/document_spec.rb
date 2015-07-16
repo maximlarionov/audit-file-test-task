c125573fe8b6a10f01134144d75fef2b8e9fd90c
maximlarionov <maxim.larionov@flatstack.com>
2015-07-16 13:16:43 +0300

    Add presenter specs

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -1,19 +1,4 @@
 require "rails_helper"
 
 describe Document do
-  let(:document) { create(:document_docx) }
-
-  subject { document }
-
-  its(:name) { is_expected.to eq "document-docx.docx" }
-  its(:extension) { is_expected.to eq ".docx" }
-  its(:content_ms_word) { is_expected.to include "ms docx file example" }
-
-  context "when document is .doc" do
-    let(:document) { create(:document_doc) }
-
-    its(:name) { is_expected.to eq "document-doc.doc" }
-    its(:extension) { is_expected.to eq ".doc" }
-    its(:content_ms_word) { is_expected.to include "ms doc file example" }
-  end
 end
