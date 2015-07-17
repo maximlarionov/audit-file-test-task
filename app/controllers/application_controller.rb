999e9060882689b74fa4384ae39fe211a125bd39
maximlarionov <maxim.larionov@flatstack.com>
2015-07-17 13:47:32 +0300

    Render 422 error if heroku deleted attachment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
+  rescue_from Errno::ENOENT, with: :record_not_found
   protect_from_forgery with: :exception
   responders :flash
 
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
+  end
+
+  private
+
+  def record_not_found
+    render file: "public/422", status: 422
   end
 end
